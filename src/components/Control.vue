e09b85dbbf3f18117dbc791f7ffb5f7fce47b537
ivc17 <ivc1741@gmail.com>
2020-09-02 10:45:53 +0800

    add controls

---
+++ src/components/Control.vue
@@ -0,0 +1,41 @@
+<template>
+  <div :class="{ controls: true, hover: !stable }">
+    <!-- eslint-disable-next-line-->
+    <router-link class="back" :to="link" v-if="!isExternal"></router-link>
+    <a class="back" :href="link" v-if="isExternal"></a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Control',
+  props: {
+    link: { type: String, required: true },
+    isExternal: { type: Boolean },
+    stable: { type: Boolean }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.controls
+  z-index: 5
+  width: 100%
+  position: fixed
+  top: 0
+  height: 100px
+
+.hover
+  opacity: 0
+  &:hover
+    opacity: 1
+
+.back
+  width: 100px
+  height: 100px
+  display: block
+  cursor: pointer
+  background-image: url('../assets/icons/backIcon.svg')
+  background-repeat: no-repeat
+  background-size: 100px 100px
+</style>
